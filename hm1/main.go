0d70677c061e254cf9b4f576ee57275f9c404c20
Eretic431 <abdullapower@outlook.com>
2020-03-08 05:24:29 +0300

    Inverted Index
    Токены тримятся по знакам пунктуации. Для каждого такена храню позицию токена в каждом документе.

---
+++ hm1/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/polisgo2020/Akhmedov_Abdulla/inverted_index"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	// os.Args[0] = path to executable file so len(os.Args) equals 1 when no arguments provided
+	if len(os.Args) == 1 {
+		log.Fatal("There are no arguments!")
+	}
+
+	// if false directory provided
+	sin := flag.Bool("s", false, "True if either single file or sequence of files provided")
+	flag.Parse()
+
+	var directories []string
+	if *sin {
+		directories = os.Args[2:] // os.Args[1] = sin
+	} else {
+		directories = os.Args[1:]
+	}
+
+	tmp := inverted_index.GetInvertedIndex(*sin, directories)
+	var str string
+	for token, documents := range tmp {
+		str += fmt.Sprintf("\"%s\": ", token)
+		i := 0
+		for document, positions := range documents {
+			str += fmt.Sprintf("%s{", document)
+			for i, position := range positions {
+				if i == len(positions)-1 {
+					str += fmt.Sprintf("%d", position)
+				} else {
+					str += fmt.Sprintf("%d, ", position)
+				}
+			}
+			if i == len(documents) - 1 {
+				str += fmt.Sprintf("} \n")
+			} else {
+				str += fmt.Sprintf("} | ")
+			}
+			i++
+		}
+	}
+
+	const PERMISSION = 0444 // read only
+	if err := ioutil.WriteFile("output.txt", []byte(str), PERMISSION); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
---
+++ inverted_index/inverted_index.go
@@ -0,0 +1,70 @@
+package inverted_index
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+	"unicode"
+)
+
+func readFiles(flag bool, files []string) map[string]string {
+	m := make(map[string]string)
+	i := 0
+
+	if flag {
+		for _, v := range files {
+			data, err := ioutil.ReadFile(v)
+			if err != nil {
+				log.Fatal(err, "Could not read file!")
+			}
+
+			tmp := strings.Split(v, "/")
+			fileName := tmp[len(tmp) - 1]
+			m[fmt.Sprint(i)+"_"+fileName] = string(data)
+			i++
+		}
+	} else {
+		for _, v := range files {
+			dir, err := ioutil.ReadDir(v)
+			if err != nil {
+				log.Fatal(err, "Could not read directory!")
+			}
+
+			for _, file := range dir {
+				data, err := ioutil.ReadFile(v + file.Name())
+				if err != nil {
+					log.Fatal(err, "Could not read file!")
+				}
+
+				m[fmt.Sprint(i)+"_"+file.Name()] = string(data)
+				i++
+			}
+		}
+	}
+
+	return m
+}
+
+// returns inverted index map that also stores position of each token in document
+func GetInvertedIndex(flag bool, files []string) map[string]map[string][]int {
+	invertedIndex := make(map[string]map[string][]int)
+	filesMap := readFiles(flag, files)
+
+	for file, str := range filesMap {
+		tokens := strings.Split(str, " ")
+		for position, token := range tokens {
+			token = strings.TrimFunc(token, func(r rune) bool {
+				return !unicode.IsLetter(r)
+			})
+
+			if invertedIndex[token] == nil {
+				invertedIndex[token] = make(map[string][]int)
+			}
+
+			invertedIndex[token][file] = append(invertedIndex[token][file], position)
+		}
+	}
+
+	return invertedIndex
+}
